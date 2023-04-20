ca513faeae4d6a484a4215be950907de92f84f09
Galadriel <galadriel.briere@gmail.com>
2023-04-20 17:33:49 +0200

    Forgot to remove colnames in bipartite files

--- GeneDiseaseAssociations/1_gene_disease_multiplexes/test_2020associations/update_bipartite.py
+++ GeneDiseaseAssociations/1_gene_disease_multiplexes/test_2020associations/update_bipartite.py
@@ -7,7 +7,7 @@ path = os.path.dirname(os.path.realpath(__file__))
 path = path + '/'
 os.chdir(path)
 
-original_bipartite = pd.read_table("networks/bipartite/1_2.gr", header=None)
+original_bipartite = pd.read_table("networks/bipartite/1_2_prop0.gr", header=None)
 original_bipartite.columns = ["gene", "disease", "score"] 
 
 last_bipartite = pd.read_table("gene_disease_associations_2020/gene_disease_2020.tsv", header=None)
@@ -22,4 +22,4 @@ uncovered.columns = ["gene", "disease", "score"]
 for prop in range(10,80,10):
     keep = uncovered.sample(n=round(len(uncovered)*prop/100))
     new_bip = pd.concat([original_bipartite, keep])
-    new_bip.to_csv("networks/bipartite/1_2_prop"+str(prop)+".gr", sep="\t", index=False, header=True)
+    new_bip.to_csv("networks/bipartite/1_2_prop"+str(prop)+".gr", sep="\t", index=False, header=False)
